dfbc09a4a1eaccfa6aaa6a9ef5ee6b23d8201415
pertunell <per.tunell@gmail.com>
2026-04-30 15:45:49 +0000

    Fix token proxy

--- api/token.js
+++ api/token.js
@@ -1,8 +1,10 @@
 export default async function handler(req, res) {
+  const body = `grant_type=client_credentials&client_id=lIclSqtjDit_jSjl5c7RtongaFIa&client_secret=3n22pRi80yVBTqnYzqp_6fc0CfYa`
+  
   const r = await fetch('https://apitest.bolagsverket.se/auth/realms/prod/protocol/openid-connect/token', {
     method: 'POST',
     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    body: req.body
+    body: body
   })
   const data = await r.json()
   res.json(data)
