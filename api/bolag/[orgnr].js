9485fa94022a576f57e978dd9c4d5cbf3096b64d
pertunell <per.tunell@gmail.com>
2026-04-30 15:51:17 +0000

    Switch to production API

--- api/bolag/[orgnr].js
+++ api/bolag/[orgnr].js
@@ -1,7 +1,8 @@
+
 export default async function handler(req, res) {
   const { orgnr } = req.query
   const token = req.headers.authorization
-  const r = await fetch('https://apitest.bolagsverket.se/vardefulladatamangder/v1/organisationer/' + orgnr, {
+  const r = await fetch('https://api.bolagsverket.se/vardefulladatamangder/v1/organisationer/' + orgnr, {
     headers: { Authorization: token }
   })
   const data = await r.json()
--- api/token.js
+++ api/token.js
@@ -1,7 +1,8 @@
+
 export default async function handler(req, res) {
-  const body = `grant_type=client_credentials&client_id=lIclSqtjDit_jSjl5c7RtongaFIa&client_secret=3n22pRi80yVBTqnYzqp_6fc0CfYa`
+  const body = `grant_type=client_credentials&client_id=H9OA9STs2SA_0LwcVTzwmYMk9bAa&client_secret=qtuhRsY_bRiXFQXZvgpXltd_8Uga`
   
-  const r = await fetch('https://apitest.bolagsverket.se/auth/realms/prod/protocol/openid-connect/token', {
+  const r = await fetch('https://api.bolagsverket.se/auth/realms/prod/protocol/openid-connect/token', {
     method: 'POST',
     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
     body: body
